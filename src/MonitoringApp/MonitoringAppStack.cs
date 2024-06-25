81ffe164117d018b4f958228a7e04c31c8d76828
subham.bk@alphaventus.com <subham.bk@alphaventus.com>
2024-06-26 08:19:25 +1000

    Initial commit

---
+++ src/MonitoringApp/MonitoringAppStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace MonitoringApp
+{
+    public class MonitoringAppStack : Stack
+    {
+        internal MonitoringAppStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
