71504774338a28550d8b1ec4d68c85b0e917d9c1
Prajwol <dhunganaprajwol237@gmail.com>
2024-08-13 16:22:22 +1000

    added subscription user and added a unit test file

--- test/monitoring_app.test.js
+++ test/monitoring_app.test.js
@@ -1,37 +1,65 @@
 const { App } = require('aws-cdk-lib');
-const { Template, Match } = require('aws-cdk-lib/assertions');
-const { MonitoringAppStack } = require('../lib/monitoring_app-stack');
+const { Template } = require('aws-cdk-lib/assertions');
+const { MonitoringAppStack } = require('../lib/monitoring_app-stack'); // Adjust path if necessary
 
-describe('MonitoringAppStack', () => {
-  let app;
-  let stack;
-
-  beforeAll(() => {
-    app = new App();
-    stack = new MonitoringAppStack(app, 'MyTestStack');
-  });
-
-  test('S3 Bucket is deployed', () => {
+test('S3 bucket is created with versioning enabled and auto-delete objects', () => {
+    const app = new App();
+    const stack = new MonitoringAppStack(app, 'TestMonitoringAppStack');
+    
     const template = Template.fromStack(stack);
+
     template.hasResourceProperties('AWS::S3::Bucket', {
-      VersioningConfiguration: {
-        Status: 'Enabled',
-      },
-    });
-  });
-
-  test('Lambda Function is deployed', () => {
-    const template = Template.fromStack(stack);
-    template.hasResourceProperties('AWS::Lambda::Function', {
-      Handler: 'index.handler',
-      Runtime: 'nodejs16.x',
-      Environment: {
-        Variables: {
-          BUCKET_NAME: {
-            Ref: Match.anyValue(),
-          },
+        VersioningConfiguration: {
+            Status: 'Enabled',
         },
-      },
     });
-  });
+
+    template.hasResource('AWS::S3::Bucket', {
+        DeletionPolicy: 'Delete',
+    });
+});
+
+test('Lambda function is created with the correct environment variables', () => {
+    const app = new App();
+    const stack = new MonitoringAppStack(app, 'TestMonitoringAppStack');
+    
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::Lambda::Function', {
+        Environment: {
+            Variables: {
+                BUCKET_NAME: {
+                    "Ref": "MyBucketF68F3FF0", 
+                },
+            },
+        },
+        Runtime: 'nodejs16.x',
+        Handler: 'index.handler',
+    });
+});
+
+test('SNS topic is created and subscriptions are added', () => {
+    const app = new App();
+    const stack = new MonitoringAppStack(app, 'TestMonitoringAppStack');
+    
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::SNS::Subscription', {
+        Protocol: 'email', // Ensure that the protocol is set correctly
+    });
+});
+
+test('CloudWatch alarms are created for availability and latency metrics', () => {
+    const app = new App();
+    const stack = new MonitoringAppStack(app, 'TestMonitoringAppStack');
+    
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::CloudWatch::Alarm', {
+        MetricName: 'WebsiteAvailability', // Updated value
+        Namespace: 'MonitoringAppNamespace', // Updated value
+        ComparisonOperator: 'LessThanThreshold',
+        Threshold: 1,
+        EvaluationPeriods: 1,
+    });
 });
