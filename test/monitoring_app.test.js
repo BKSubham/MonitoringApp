2058a836f9ae8eb52f0fb5eeb180b3b951394158
Prajwol <dhunganaprajwol237@gmail.com>
2024-08-14 17:18:55 +1000

    Added test and pipeline

--- test/monitoring_app.test.js
+++ test/monitoring_app.test.js
@@ -1,17 +1,65 @@
-// const cdk = require('aws-cdk-lib');
-// const { Template } = require('aws-cdk-lib/assertions');
-// const MonitoringApp = require('../lib/monitoring_app-stack');
+const { App } = require('aws-cdk-lib');
+const { Template } = require('aws-cdk-lib/assertions');
+const { MonitoringAppStack } = require('../lib/monitoring_app-stack'); // Adjust path if necessary
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/monitoring_app-stack.js
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//   // WHEN
-//   const stack = new MonitoringApp.MonitoringAppStack(app, 'MyTestStack');
-//   // THEN
-//   const template = Template.fromStack(stack);
+test('S3 bucket is created with versioning enabled and auto-delete objects', () => {
+    const app = new App();
+    const stack = new MonitoringAppStack(app, 'TestMonitoringAppStack');
+    
+    const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    template.hasResourceProperties('AWS::S3::Bucket', {
+        VersioningConfiguration: {
+            Status: 'Enabled',
+        },
+    });
+
+    template.hasResource('AWS::S3::Bucket', {
+        DeletionPolicy: 'Delete',
+    });
+});
+
+test('Lambda function is created with the correct environment variables', () => {
+    const app = new App();
+    const stack = new MonitoringAppStack(app, 'TestMonitoringAppStack');
+    
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::Lambda::Function', {
+        Environment: {
+            Variables: {
+                BUCKET_NAME: {
+                    "Ref": "MyBucketF68F3FF0", 
+                },
+            },
+        },
+        Runtime: 'nodejs16.x',
+        Handler: 'index.handler',
+    });
+});
+
+test('SNS topic is created and subscriptions are added', () => {
+    const app = new App();
+    const stack = new MonitoringAppStack(app, 'TestMonitoringAppStack');
+    
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::SNS::Subscription', {
+        Protocol: 'email', // Ensure that the protocol is set correctly
+    });
+});
+
+test('CloudWatch alarms are created for availability and latency metrics', () => {
+    const app = new App();
+    const stack = new MonitoringAppStack(app, 'TestMonitoringAppStack');
+    
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::CloudWatch::Alarm', {
+        MetricName: 'WebsiteAvailability', // Updated value
+        Namespace: 'MonitoringAppNamespace', // Updated value
+        ComparisonOperator: 'LessThanThreshold',
+        Threshold: 1,
+        EvaluationPeriods: 1,
+    });
 });
