90344ce0f33f598f6a2b3dc105a4d0514230e60a
subham.bk@alphaventus.com <subham.bk@alphaventus.com>
2024-08-21 10:21:49 +1000

    pipeline, notification user added  and env declared in bin stack

--- bin/monitoring_app.js
+++ bin/monitoring_app.js
@@ -6,7 +6,12 @@ const { PipelineStack } = require('../lib/pipeline-stack');
 
 
 const app = new cdk.App();
-new PipelineStack(app, 'PipelineStack');
+new PipelineStack(app, 'PipelineStack',{
+  env: {
+    region: 'us-east-1', 
+    account: '975049953018', 
+  },
+});
 
 new MonitoringAppStack(app, 'MonitoringAppStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
@@ -20,7 +25,10 @@ new MonitoringAppStack(app, 'MonitoringAppStack', {
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
-
+  env: {
+    region: 'us-east-1', 
+    account: '975049953018', 
+  },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
 
